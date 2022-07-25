1fd4e8a6340ff078af0dfc7cf75fd76257a3e335
FacuBobarini <facubobarini@gmail.com>
2022-07-25 16:31:13 -0300

    add validationResult

--- src/movies/movies.controller.js
+++ src/movies/movies.controller.js
@@ -2,6 +2,12 @@ const { findAllMovies } = require('./movies.service');
 
 async function getMovies(req, res) {
   try {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.status(422).json({ errors: errors.array() });
+      return;
+    }
     findMovie = await findAllMovies(req.query);
     res.status(200).json(findMovie);
   } catch (error) {
