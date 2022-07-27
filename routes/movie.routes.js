8d5bcc56ed4b63d6ae75b89f9bca91f5750ac1c4
FacuBobarini <facubobarini@gmail.com>
2022-07-27 18:01:12 -0300

    add route movie/:uuid

--- routes/movie.routes.js
+++ routes/movie.routes.js
@@ -1,7 +1,11 @@
-const { validate } = require('../src/middleware/validations');
-const { getMovies } = require('../src/movies/movies.controller');
+const {
+  validateMovies,
+  validateMoviesById,
+} = require('../src/middleware/validations.movies');
+const { getMovies, getMoviesById } = require('../src/movies/movies.controller');
 const router = require('express').Router();
 
-router.get('/movies', validate('findMovies'), getMovies);
+router.get('/movies', validateMovies(), getMovies);
+router.get('/movies/:uuid', validateMoviesById(), getMoviesById);
 
 module.exports = router;
