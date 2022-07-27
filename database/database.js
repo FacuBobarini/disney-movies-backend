cf02ee681c5f49e68505e2a1ac75539d743aba34
FacuBobarini <facubobarini@gmail.com>
2022-07-27 17:59:08 -0300

    delete function initializeDatabase and export databaseConnection- bring this changes to index

--- database/database.js
+++ database/database.js
@@ -2,37 +2,31 @@ const { Sequelize } = require('sequelize');
 const { characterModel } = require('./models/character');
 const { genreModel } = require('./models/genre');
 const { movieModel } = require('./models/movie');
+const dotenv = require('dotenv');
+dotenv.config();
 
-async function initializeDatabase() {
-  try {
-    const databaseConnection = await new Sequelize(
-      process.env.DB_NAME,
-      process.env.DB_USER,
-      process.env.DB_PASS,
-      {
-        host: 'localhost',
-        dialect: 'postgres',
-      }
-    );
-
-    const movie = databaseConnection.define('Movie', movieModel);
-    const character = databaseConnection.define('Character', characterModel);
-    const genre = databaseConnection.define('Genre', genreModel);
-
-    movie.belongsToMany(character, { through: 'characterMovies' });
-    character.belongsToMany(movie, { through: 'characterMovies' });
-
-    movie.belongsToMany(genre, { through: 'moviesGenre' });
-    genre.belongsToMany(movie, { through: 'moviesGenre' });
-
-    (async () => {
-      await databaseConnection.sync({ force: false });
-    })();
-
-    return databaseConnection;
-  } catch (error) {
-    console.error(error);
+const databaseConnection = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: 'localhost',
+    dialect: 'postgres',
   }
-}
+);
 
-module.exports = { initializeDatabase };
+const movie = databaseConnection.define('Movie', movieModel);
+const character = databaseConnection.define('Character', characterModel);
+const genre = databaseConnection.define('Genre', genreModel);
+
+movie.belongsToMany(character, { through: 'characterMovies' });
+character.belongsToMany(movie, { through: 'characterMovies' });
+
+movie.belongsToMany(genre, { through: 'moviesGenre' });
+genre.belongsToMany(movie, { through: 'moviesGenre' });
+
+(async () => {
+  await databaseConnection.sync({ force: false });
+})();
+
+module.exports = { databaseConnection };
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const { initializeDatabase } = require('./database/database');
+const { databaseConnection } = require('./database/database');
 const movieRouter = require('./routes/movie.routes');
 
 async function bootstrap() {
   try {
     dotenv.config();
-    const dbConnection = initializeDatabase();
+    const dbConnection = databaseConnection;
     const app = express();
     app.use(express.json());
     app.use('/api', movieRouter);
