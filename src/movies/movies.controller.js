701faf66d7768c16d476e66aef14ebc6bf60d14a
FacuBobarini <facubobarini@gmail.com>
2022-09-05 23:01:11 -0300

    add function: addmovie

--- src/movies/movies.controller.js
+++ src/movies/movies.controller.js
@@ -16,7 +16,7 @@ async function getMovies(req, res) {
   }
 }
 
-async function getMoviesById(req, res) {
+async function getMovieById(req, res) {
   try {
     const errors = validationResult(req);
 
@@ -31,4 +31,17 @@ async function getMoviesById(req, res) {
   }
 }
 
-module.exports = { getMovies, getMoviesById };
+async function addMovie(req, res) {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const newMovie = await createMovie(req.body);
+    res.status(200).json(newMovie);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+}
+
+module.exports = { getMovies, getMovieById, addMovie };
