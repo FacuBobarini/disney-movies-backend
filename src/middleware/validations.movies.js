e1972fd7ec39cc9df4150e158d02ebc95b190313
FacuBobarini <facubobarini@gmail.com>
2022-09-05 23:00:26 -0300

    add validatios to addmovie route

--- src/middleware/validations.movies.js
+++ src/middleware/validations.movies.js
@@ -13,4 +13,18 @@ function validateMoviesById() {
   const order = ['ASC', 'DESC'];
   return [param('uuid').exists({ checkFalsy: true }).isUUID(4)];
 }
-module.exports = { validateMovies, validateMoviesById };
+
+function addMovieInputValidator() {
+  return [
+    body('image').exists({ checkFalsy: true }).isURL().optional(),
+    body('title').exists({ checkFalsy: true }).optional(),
+    body('creationDate').exists({ checkFalsy: true }).isDate().optional(),
+    body('rate')
+      .exists({ checkFalsy: true })
+      .isInt({ min: 1, max: 5 })
+      .optional(),
+    body('charUuid.*').exists({ checkFalsy: true }).isUUID(4).optional(),
+    body('genreUuid.*').exists({ checkFalsy: true }).isUUID(4).optional(),
+  ];
+}
+module.exports = { validateMovies, validateMoviesById, addMovieInputValidator };
