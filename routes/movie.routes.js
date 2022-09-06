b7a26a102b946f42da0a8d368fd617c48a7b7b0f
FacuBobarini <facubobarini@gmail.com>
2022-09-05 23:12:10 -0300

    add route updatemovie

--- routes/movie.routes.js
+++ routes/movie.routes.js
@@ -2,16 +2,19 @@ const {
   validateMovies,
   validateMoviesById,
   addMovieInputValidator,
+  updateMovieInputValidator,
 } = require('../src/middleware/validations.movies');
 const {
   getMovies,
   getMovieById,
   addMovie,
+  updateMovie,
 } = require('../src/movies/movies.controller');
 const router = require('express').Router();
 
 router.get('/movies', validateMovies(), getMovies);
 router.get('/movies/:uuid', validateMoviesById(), getMovieById);
 router.post('/movies', addMovieInputValidator(), addMovie);
+router.patch('/movies/:uuid', updateMovieInputValidator(), updateMovie);
 
 module.exports = router;
