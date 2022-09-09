99f84efb616d57f459c7afb5fc7ee9fdf9937b3d
FacuBobarini <facubobarini@gmail.com>
2022-09-08 23:52:04 -0300

    add route getCharacterById

--- routes/character.routes.js
+++ routes/character.routes.js
@@ -1,9 +1,16 @@
-const { getCharacters } = require('../src/characters/characters.controller');
+const {
+  getCharacters,
+  getCharacterById,
+} = require('../src/characters/characters.controller');
 const {
   getCharactersInputValidation,
 } = require('../src/characters/characters.inputValidations');
+const {
+  uuidParamsInputValidator,
+} = require('../src/movies/validations.movies');
 const router = require('express').Router();
 
 router.get('/characters', getCharactersInputValidation(), getCharacters);
+router.get('/characters/:uuid', uuidParamsInputValidator(), getCharacterById);
 
 module.exports = router;
