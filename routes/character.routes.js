2a4b24325bdd4290b0558a0e05eeffbf780a9d06
FacuBobarini <facubobarini@gmail.com>
2022-09-10 17:27:54 -0300

    add route to add new characters in path /characters

--- routes/character.routes.js
+++ routes/character.routes.js
@@ -1,16 +1,17 @@
 const {
   getCharacters,
   getCharacterById,
+  addCharacter,
 } = require('../src/characters/characters.controller');
 const {
   getCharactersInputValidation,
-} = require('../src/characters/characters.inputValidations');
-const {
   uuidParamsInputValidator,
-} = require('../src/movies/validations.movies');
+  addCharacterInputValidator,
+} = require('../src/characters/characters.inputValidations');
 const router = require('express').Router();
 
 router.get('/characters', getCharactersInputValidation(), getCharacters);
 router.get('/characters/:uuid', uuidParamsInputValidator(), getCharacterById);
+router.post('/characters', addCharacterInputValidator(), addCharacter);
 
 module.exports = router;
