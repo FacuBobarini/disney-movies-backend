cec5364dd1b0ba462ff5b9fd1ef014cb9c4fe0df
FacuBobarini <facubobarini@gmail.com>
2022-09-12 22:48:35 -0300

    add route to update characters in path /characters/:uuid

--- routes/character.routes.js
+++ routes/character.routes.js
@@ -2,16 +2,23 @@ const {
   getCharacters,
   getCharacterById,
   addCharacter,
+  updateCharacter,
 } = require('../src/characters/characters.controller');
 const {
   getCharactersInputValidation,
   uuidParamsInputValidator,
   addCharacterInputValidator,
+  updateCharacterInputValidator,
 } = require('../src/characters/characters.inputValidations');
 const router = require('express').Router();
 
 router.get('/characters', getCharactersInputValidation(), getCharacters);
 router.get('/characters/:uuid', uuidParamsInputValidator(), getCharacterById);
 router.post('/characters', addCharacterInputValidator(), addCharacter);
+router.patch(
+  '/characters/:uuid',
+  updateCharacterInputValidator(),
+  updateCharacter
+);
 
 module.exports = router;
