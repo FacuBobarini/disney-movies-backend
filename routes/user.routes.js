76f76b9dd4688cd6f2fef36766381e826556a780
FacuBobarini <facubobarini@gmail.com>
2022-09-14 22:16:25 -0300

    add route to add new user- path /auth/register

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,3 +1,8 @@
+const { addUser } = require('../src/users/users.controller');
+const {
+  addUserInputValidator,
+} = require('../src/users/users.inputValidations');
+
 const router = require('express').Router();
 
-router.post('/auth/register');
+router.post('/auth/register', addUserInputValidator(), addUser);
