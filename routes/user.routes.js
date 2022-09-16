5911cb58469f2d4b4a0877a1adde096b33214830
FacuBobarini <facubobarini@gmail.com>
2022-09-15 22:51:34 -0300

    add route to login w/path: /auth/login

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,8 +1,13 @@
-const { addUser } = require('../src/users/users.controller');
+const { addUser, loginUser } = require('../src/users/users.controller');
 const {
   addUserInputValidator,
+  loginUserInputValidator,
 } = require('../src/users/users.inputValidations');
 
 const router = require('express').Router();
 
 router.post('/auth/register', addUserInputValidator(), addUser);
+
+router.post('/auth/login', loginUserInputValidator(), loginUser);
+
+module.exports = router;
